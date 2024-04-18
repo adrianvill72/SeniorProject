5b3413484db62c7a277dec9a2d685b0a76198054
Adrian <adrianvill72@gmail.com>
2024-04-18 17:20:18 -0500

    updating my branch

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -1,9 +1,6 @@
-import {signOut} from "firebase/auth"
-import {Link, useNavigate} from "react-router-dom";
 import EventPage from './components/Homepage/EventPage';
 import NavBar from "./components/Homepage/NavBar";
-import {useAuth}  from './firebase';
-import {userNavBar,guestNavBar} from "./components/Homepage/NavBar";
+
 
 const Home = () => {
     return (
