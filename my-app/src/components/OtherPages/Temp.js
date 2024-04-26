f29a29460114b4c85c227ba188fff533ead8de5c
Adrian <adrianvill72@gmail.com>
2024-04-25 20:22:12 -0500

    updated images, remember me, forgot password, and drop down menu

---
+++ my-app/src/components/OtherPages/Temp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Temp() {
+    return (
+        <div>
+            <h1>Temp Profile Page</h1>
+            <p>This is a temporary profile page.</p>
+        </div>
+    );
+}
+
+export default Temp;
